d2c4752623d05418fe7e059fc55482eee020d323
Andrea Miglietta <m.andrea96@hotmail.it>
2022-10-23 23:11:14 +0200

    file album.php e inclusione e formattazione

---
+++ album.php
@@ -0,0 +1,26 @@
+<div class="album">
+    <!-- cover album-->
+    <div class="mb-3">
+        <img src="<?= $songs['cover']?>" alt="<?= $songs['title']?>">
+    </div>
+
+    <!-- titolo canzone -->
+    <h3 class="text-uppercase">
+        <?= $songs['title']?>
+    </h3>
+
+    <!-- album -->
+    <p class="lh-1 mb-1">
+        <?= $songs['album']?>
+    </p>
+
+    <!-- autore -->
+    <p class="lh-1 mb-1">
+        <?= $songs['author']?>
+    </p>
+
+    <!-- anno -->
+    <p class="lh-1 mb-1">
+        <?= $songs['year']?>
+    </p>
+</div>
--- index.php
+++ index.php
@@ -5,22 +5,35 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link rel="stylesheet" href="style/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>Dischi</title>
 </head>
 <body>
 
-<div class="container">
-    <div class="songs-container">
-        <?php
-            include __DIR__ . '/dischi.php';
-        
-            foreach($databaseDischi as $songs){
+    <!-- container canzoni -->
+    <div class="container">
+        <div class="songs-container d-flex flex-wrap">
+            <?php
+                // inclusione database canzoni
+                include __DIR__ . '/dischi.php';
+            
+                foreach($databaseDischi as $songs){
 
-            }
-        ?>
+            ?>
+
+            <div>
+                <?php
+                    // inclusione formattazione album
+                    include __DIR__ . '/album.php'
+                ?>
+            </div>
+
+            <?php
+                }
+            ?>
+        </div>
     </div>
-</div>
 </body>
 </html>
