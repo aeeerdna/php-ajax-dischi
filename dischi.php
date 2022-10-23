7603c246427914890b2ccb6b5b8de985a2fb9367
Andrea Miglietta <m.andrea96@hotmail.it>
2022-10-23 18:00:28 +0200

    creazione database canzoni

---
+++ dischi.php
@@ -0,0 +1,55 @@
+<?php
+
+$databaseDischi = [
+    [
+        'title' => 'Not Afraid',
+        'album' => 'Recovery',
+        'author' => 'Eminem',
+        'year' => '2010',
+        'cover' => 'https://upload.wikimedia.org/wikipedia/en/6/60/Recovery_Album_Cover.jpg',
+    ],
+    
+    [
+        'title' => 'Kiss It Better',
+        'album' => 'Anti',
+        'author' => 'Rihanna',
+        'year' => '2016',
+        'cover' => 'https://upload.wikimedia.org/wikipedia/en/3/32/Rihanna_-_Anti.png',
+    ],
+
+    [
+        'title' => 'Mirror',
+        'album' => 'Tha Carter IV',
+        'author' => 'Lil Wayne',
+        'year' => '2011',
+        'cover' => 'https://upload.wikimedia.org/wikipedia/en/d/d1/Lil_Wayne_-_Tha_Carter_IV.jpg',
+    ],
+
+    [
+        'title' => 'Sticky',
+        'album' => 'Honestly, Nevermind',
+        'author' => 'Drake',
+        'year' => '2022',
+        'cover' => 'https://upload.wikimedia.org/wikipedia/en/c/c7/Honestly%2C_Nevermind_-_Drake.png',
+    ],
+
+    [
+        'title' => 'So Sophisticated',
+        'album' => "God Forgives, I Don't",
+        'author' => 'Rick Ross',
+        'year' => '2012',
+        'cover' => 'https://upload.wikimedia.org/wikipedia/en/2/2f/GodForgivesIDont.PNG',
+    ],
+
+    [
+        'title' => 'Still D.R.E.',
+        'album' => '2001',
+        'author' => 'Dr.Dre',
+        'year' => '1999',
+        'cover' => 'https://upload.wikimedia.org/wikipedia/en/5/56/DrDre-2001.jpg',
+    ],
+
+    
+];
+
+?>
